455b39bda6799b98edc04cb4db2093a0da7e9036
Vruddy <ruddy.villain@epitech.eu>
2025-06-06 09:40:53 +0200

    add ignore files, edit ignore for android, ios

---
+++ android/app/src/main/java/com/ressources/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ressources.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ressources.app',
+  appName: 'ressources',
+  webDir: 'out'
+};
+
+export default config;
