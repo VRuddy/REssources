665f405813629c14c0360fd53a8be7f59384a42d
ergys <ergys.dushaj@viacesi.fr>
2025-06-30 16:18:57 +0200

    test

--- src/components/sign-up-form.tsx
+++ src/components/sign-up-form.tsx
@@ -52,6 +52,7 @@ export function SignUpForm({ className, ...props }: React.ComponentPropsWithoutR
       setIsLoading(false)
     }
   }
+  
 
   return (
     <div className={cn('flex flex-col gap-6', className)} {...props}>
